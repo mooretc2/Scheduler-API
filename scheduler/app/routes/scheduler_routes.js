82bdc2bb86bf940a9dab029df29eaedd0bd52355
Tyler Moore <mooretc2@miamioh.edu>
2017-10-03 16:07:32 +0000

    fixing set methods because I'm bad

--- scheduler/app/routes/scheduler_routes.js
+++ scheduler/app/routes/scheduler_routes.js
@@ -8,7 +8,7 @@ module.exports = function(app, db) {
 		res.send('hw success');
 	});
 	app.get('/sethw/:time', (req, res) => {
-		res.send('set hw success ' + req.params);
+		res.send('set hw success ' + req.params.time);
 	});
 	app.get('/sleep', (req, res) => {
 		res.send('sleep success');
@@ -17,6 +17,6 @@ module.exports = function(app, db) {
 		res.send('food success');
 	});
 	app.get('/setrequired/:time', (req, res) => {
-		res.send('set required success ' + req.params);
+		res.send('set required success ' + req.params.time);
 	});
 };
