4f66beba4c846cab273e3535b6feefde844aa58e
mooretc2 <mooretc2@miamioh.edu>
2017-10-03 13:05:47 -0400

    clockOut function started

--- scheduler/app/routes/scheduler_routes.js
+++ scheduler/app/routes/scheduler_routes.js
@@ -1,13 +1,22 @@
 var util = require('util');
+var duration = require('duration');
+var hw, sleep, food, required;
+var remhw, remsleep, remfood;
+var totalhw, totalsleep, totalfood, totalrequired, totalfree;
+var lastClock, activity;
 
 module.exports = function(app, db) {
 	app.get('/free', (req, res) => {
 		res.send('Success');
 	});
+	app.get('/remfree', (req, res) => {
+		res.send('remaining free success');
+	});
 	app.get('/hw', (req, res) => {
 		res.send('hw success');
 	});
 	app.get('/sethw/:time', (req, res) => {
+		getActivity();
 		res.send('set hw success ' + req.params.time);
 	});
 	app.get('/sleep', (req, res) => {
@@ -16,7 +25,35 @@ module.exports = function(app, db) {
 	app.get('/food', (req, res) => {
 		res.send('food success');
 	});
+	app.get('/setfood/:time', (req, res) => {
+		res.send('set food success ' + req.params.time);
+	});
 	app.get('/setrequired/:time', (req, res) => {
 		res.send('set required success ' + req.params.time);
 	});
 };
+
+function clockOut(){
+	if(activity == 'sleep'){
+		totalSleep += getDiff();
+		if(Date.now().hour >= 22 || Date.now().hour <= 10){
+			newDay();
+		}
+	}
+	if(activity == 'food')
+		totalFood += getDiff();
+	if(activity == 'hw')
+		return hw;
+	if(activity == 'required')
+		return required;
+};
+
+function getDiff() {
+	var diff = new duration(lastClock, Date.now());
+	lastClock = Date.now();
+	return diff.hour * 60 + diff.minute;
+};
+
+function newDay() {
+	return;
+}
