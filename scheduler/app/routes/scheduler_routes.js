f724259d17d986ef3cd003b6ac3b39ca3a275358
Tyler Moore <mooretc2@miamioh.edu>
2017-10-03 15:35:34 +0000

    Hello World for each function complete

--- scheduler/app/routes/scheduler_routes.js
+++ scheduler/app/routes/scheduler_routes.js
@@ -1,7 +1,16 @@
 var util = require('util');
 
 module.exports = function(app, db) {
-	app.get('/', (req, res) => {
+	app.get('/free', (req, res) => {
 		res.send('Success');
+	});
+	app.get('/hw', (req, res) => {
+		res.send('hw success');
+	});
+	app.get('/sleep', (req, res) => {
+		res.send('sleep success');
+	});
+	app.get('/food', (req, res) => {
+		res.send('food success');
 	});
 };
