13fd6111cbec1e6237c392abe03af00b7e400f35
Tyler Moore <mooretc2@miamioh.edu>
2017-10-10 17:40:27 -0400

    Cleaned up sets, set activity var after clock out

--- scheduler/app/routes/scheduler_routes.js
+++ scheduler/app/routes/scheduler_routes.js
@@ -8,37 +8,43 @@ var lastClock, activity;
 module.exports = function(app, db) {
 	app.get('/free', (req, res) => {
 		clockOut();
+		activity = 'free';
 		res.end();
 	});
 	app.get('/remfree', (req, res) => {
 		res.send('remaining free success');
 	});
 	app.get('/hw', (req, res) => {
-		res.send('hw success');
+		clockOut();
+		activity = 'hw';
 	});
 	app.get('/sethw/:time', (req, res) => {
 		hw = req.params.time;
-		res.end();
+		res.end('hw = ' + req.params.time);
 	});
 	app.get('/sleep', (req, res) => {
 		clockOut();
 		newDay();
-		res.send('sleep success');
+		activity = 'sleep';
+		res.end(activity);
 	});
 	app.get('/nap', (req, res) => {
 		clockOut();
-		res.end();
+		activity = 'sleep';
+		res.end(activity);
 	});
 	app.get('/food', (req, res) => {
 		clockOut();
-		res.end();
+		activity = 'food';
+		res.end(activity);
 	});
 	app.get('/setfood/:time', (req, res) => {
 		food = req.params.time;
-		res.end();
+		res.end('food = ' + req.params.time);
 	});
 	app.get('/setrequired/:time', (req, res) => {
 		required = req.params.time;
+		res.end('required = ' + req.params.time)
 	});
 };
 
